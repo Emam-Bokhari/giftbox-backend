c2e82afd0428d24d121f37b3a045ce3c1a1b13b3
Moshfiqur Rahman <studentemam@gmail.com>
2026-06-13 15:12:12 +0600

    feat: change redis

--- src/config/bull.ts
+++ src/config/bull.ts
@@ -1,20 +1,37 @@
+// import IORedis from "ioredis";
+
+// export const connection = new IORedis({
+//   host: "127.0.0.1",
+//   port: 6379,
+//   maxRetriesPerRequest: null,
+// });
+
+// export const defaultJobOptions = {
+//   attempts: 3,
+//   backoff: {
+//     type: "exponential",
+//     delay: 2000,
+//   },
+//   // removeOnComplete: true,
+//   removeOnComplete: {
+//     age: 3600,
+//   },
+//   removeOnFail: false,
+// };
+
 import IORedis from "ioredis";
 
-export const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null,
+export const connection = new IORedis(
+  process.env.REDIS_URL as string,
+  {
+    maxRetriesPerRequest: null,
+  }
+);
+
+connection.on("connect", () => {
+  console.log("✅ Redis connected");
 });
 
-export const defaultJobOptions = {
-  attempts: 3,
-  backoff: {
-    type: "exponential",
-    delay: 2000,
-  },
-  // removeOnComplete: true,
-  removeOnComplete: {
-    age: 3600,
-  },
-  removeOnFail: false,
-};
+connection.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
